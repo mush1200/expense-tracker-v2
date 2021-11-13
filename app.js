26df7bb7a973f8bb9125dc2ef7922676a0faa7c4
mush1200 <mush1200@hotmail.com>
2021-11-13 22:25:59 +0800

    add gitignore

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const app = express()
+const port = 3000
+
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.post('/', (req, res) => {
+  const options = req.body
+  const password = generatePassword(options)
+  res.render('index', {password: password, options: options})
+})
+app.listen(port, () => {
+  console.log(`The Express server is running on http://localhost:${port}.`)
+})
