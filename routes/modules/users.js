273aa120de835fb4ee289366e5a5e3ff4c65a22e
mush1200 <mush1200@hotmail.com>
2021-11-14 15:41:40 +0800

    feat: add register page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -4,5 +4,8 @@ const router = express.Router()
 router.get('/login', (req, res) => {
   return res.render('login')
 })
+router.get('/register', (req, res) => {
+  return res.render('register')
+})
 
 module.exports = router
