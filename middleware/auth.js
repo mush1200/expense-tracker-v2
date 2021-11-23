a24898403566f6dd34743733860823ffa222bfd0
ioriayaka <ioriayaka@hotmail.com>
2021-11-23 21:18:05 +0800

    feat: add admin pages and authenticatedAdmin

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,9 +1,19 @@
 module.exports = {
   authenticator: (req, res, next) => {
     if (req.isAuthenticated()) {
-      return next()
+      if (req.user.isAdmin !== '1') { return next() }
+      req.flash('error_messages', '此帳號為管理者帳號，不可登入前台！')
+      return res.redirect('/admin/signin')
     }
     req.flash('error_messages', '請先登入才能使用!')
     res.redirect('/users/login')
+  },
+    authenticatedAdmin: (req, res, next) => {
+    if (req.isAuthenticated()) {
+      if (req.user.isAdmin === '1') { return next() }
+      req.flash('error_messages', '此帳號非管理者帳號！')
+      return res.redirect('/admin/signin')
+    }
+    res.redirect('/admin/signin')
   }
 }
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -3,51 +3,43 @@ const adminController = require('../../controllers/adminController.js')
 const express = require('express')
 const balanceController = require('../../controllers/balanceController')
 const router = express.Router()
-const { authenticator } = require('../../middleware/auth')
+const { authenticator, authenticatedAdmin } = require('../../middleware/auth')
 
 
-router.get('/expense/records/filter', moneyController.getFilteredExpense)
+router.get('/expense/records/filter', authenticator, moneyController.getFilteredExpense)
 
-router.get('/:index/records/new', moneyController.newPage)
+router.get('/:index/records/new', authenticator, moneyController.newPage)
 
-router.post('/expense/records', moneyController.createExpense)
+router.post('/expense/records', authenticator, moneyController.createExpense)
 
-router.get('/:index/records/:id/edit', moneyController.editPage)
+router.get('/:index/records/:id/edit', authenticator, moneyController.editPage)
 
-router.put('/expense/records/:id',moneyController.putExpense)
+router.put('/expense/records/:id', authenticator, moneyController.putExpense)
 
-router.delete('/records/:id', moneyController.deleteExpense)
+router.delete('/records/:id', authenticator, moneyController.deleteExpense)
 
-router.get('/income', moneyController.getIncome)
+router.get('/income', authenticator, moneyController.getIncome)
 
-router.get('/income/records/filter', moneyController.getFilteredIncome)
+router.get('/income/records/filter', authenticator, moneyController.getFilteredIncome)
 
-router.get('/income/records/new', moneyController.newPage)
+router.get('/income/records/new', authenticator, moneyController.newPage)
 
-router.post('/income/records', moneyController.createIncome)
+router.post('/income/records', authenticator, moneyController.createIncome)
 
-router.put('/income/records/:id', moneyController.putIncome)
+router.put('/income/records/:id', authenticator, moneyController.putIncome)
 
-router.get('/balance', balanceController.balancePage)
+router.get('/balance', authenticator, balanceController.balancePage)
 
 // admin
 
-const authenticatedAdmin = (req, res, next) => {
-  console.log(req.isAuthenticated())
-  console.log(req.user)
-  if (req.isAuthenticated()) {
-    if (req.user.isAdmin === '1') {
-      console.log(req.user.isAdmin)
-      return next() }
-    req.flash('error_messages', '此帳號非管理者帳號！')
-    return res.redirect('/')
-  }
-  res.redirect('/admin/signin')
-}
 router.get('/admin/signin', adminController.signInPage)
 router.post('/admin/login', adminController.login)
 router.get('/admin/logout', adminController.logout)
 router.get('/admin/index', authenticatedAdmin, adminController.adminPage)
+
+router.get('/admin/users/income', authenticatedAdmin, adminController.getUserIncomeRating)
+router.get('/admin/category/income', authenticatedAdmin, adminController.getCatogryincomeRating)
+router.get('/admin/category/expense', authenticatedAdmin, adminController.getCatogryexpenseRating)
 router.get('/', authenticator, moneyController.getExpense)
 
 module.exports = router
